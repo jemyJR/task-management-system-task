538025eb8b6377f6495a4ee6c80ce758cab5d0e0
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2026-06-23 18:26:53 +0300

    Task1: Add Dockerfile and docker-compose configuration for application setup

--- src/constants/db.constants.js
+++ src/constants/db.constants.js
@@ -1,8 +1,9 @@
 import config from "../shared/config/config.js";
 
 export const MONGO_URL =
+  config.MONGO_URI ||
   `mongodb+srv://${config.DB_USER}:` +
-  `${config.DB_PASSWORD}@` +
-  `${config.DB_HOST}/` +
-  `${config.NODE_ENV}?` +
-  `${config.DB_OPTIONS}`;
+    `${config.DB_PASSWORD}@` +
+    `${config.DB_HOST}/` +
+    `${config.NODE_ENV}?` +
+    `${config.DB_OPTIONS}`;
